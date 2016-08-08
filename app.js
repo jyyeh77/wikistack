917e529f1e0a89b164f65d663a6e3c4afa517b8a
Eliot S <eliotpszw@gmail.com>
2016-08-08 14:35:25 -0400

    Initializing

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var swig = require('swig');
+var morgan = require('morgan');
+var router = require('./routes');
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+var socketio = require('socket.io');
+
+
+// templating boilerplate setup
+// point res.render to the proper directory
+app.set('views', __dirname + '/views');
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files
+// have it use swig to do so
+app.engine('html', swig.renderFile);
+// turn of swig's caching
+swig.setDefaults({cache: false});
+
+// logging middleware
+app.use(morgan('dev'));
+
+// body parsing middleware
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json()); // would be for AJAX requests
+
+var server = app.listen(1337, function(){
+ console.log('listening on port 1337');
+});
+
+app.use(express.static('public'));
